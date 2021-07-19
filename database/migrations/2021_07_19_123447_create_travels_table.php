83ea2b1e026e1160e46bef731e339e27cfd06082
Lorenzo-Calzi <lorenzocalzi@gmail.com>
2021-07-19 15:26:20 +0200

    Aggiunta Migration

---
+++ database/migrations/2021_07_19_123447_create_travels_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->string('image');
+            $table->string('destination');
+            $table->decimal('price');
+            $table->date('departure_date');
+            $table->date('return_date');
+            $table->string('place');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
---
+++ database/seeds/TravelsSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use App\Travel;
+use Faker\Generator as Faker;
+use Illuminate\Database\Seeder;
+
+class TravelsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i<10; $i++) { 
+            $travel = new Travel;
+            $travel->image = $faker->imageUrl(400, 200, 'animals', true);
+            $travel->destination = $faker->randomElement('Barcellona', 'Pag', 'Ibiza', 'Gallipoli', 'Mykonos', 'Corfù', 'Rimini', 'Malta', 'Zante', 'Formentera');
+            $travel->price = $faker->numberBetween(400, 1000);
+            $travel->departure_date = $faker->dateTimeBetween('-1 week', '+1 week');
+            $travel->return_date = $faker->dateTimeBetween('-1 week', '+1 week');
+            $travel->place = $faker->randomElement('Milano', 'Roma', 'Napoli');
+            $travel->description = $faker->paragraph();
+            $travel->save();
+        }
+    }
+}
